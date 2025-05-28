34671b4bba7a9db92d197dd97271298e9ce7e719
unknown <donwanhickey@gmail.com>
2025-05-28 08:58:49 -0400

    inital commit

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 /******************
  * YOUR CODE HERE *
- ******************
+ *******************/
+
+function getFirstItemFrom(array) {
+  return array[0];
+}
+
+function getLastItemFrom(array) {
+  return array[array.length -1];
+}
+
+function getIndex3(array) {
+  if(array.length > 3) {
+    return array[3];
+    
+  } else {
+    return array[array.length - 1];
+  }
+}
+
+function isLongList(array) {
+  return array.length >= 10;
+}
+
+function firstItemIsNumber(array) {
+  return typeof array[0] === 'number';
+}
+
+function secondCharOfThirdString(array) {
+  const thirdItem = array[2];
+
+  if (typeof thirdItem === 'string' && thirdItem.length >=2) {
+    return thirdItem[1];
+  } else {
+    return undefined
+  }
+}
+
+
+
 
 
 
